234b6ecfe4b4833063f2c757edbcbb83fe0e7241
Chamath Palihawadana <chamathpali123@gmail.com>
2020-10-23 01:19:24 +0100

    Load all the screenshots from the folder

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const path = require('path');
+const fs = require('fs');
+
+const original_path = path.join(__dirname, 'screenshots');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  let images = []
+  fs.readdir(original_path, function (err, files) {
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
+    }
+    files.forEach(function (file) {
+      images.push(file)
+      console.log(file);
+    });
+    console.log(images)
+
+    res.json(files)
+
+  });
+  // res.send('Hello World!')
 })
 
 app.listen(port, () => {
